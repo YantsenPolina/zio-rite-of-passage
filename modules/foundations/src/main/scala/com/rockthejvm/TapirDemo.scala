0562d0682263165d5835b100e875bb351f01ceff
Polina Yantsen <polinayantsen@gmail.com>
2025-10-12 12:36:24 +0200

    Add TapirDemo.

---
+++ modules/foundations/src/main/scala/com/rockthejvm/TapirDemo.scala
@@ -0,0 +1,99 @@
+package com.rockthejvm
+
+import sttp.tapir.*
+import sttp.tapir.generic.auto.*
+import sttp.tapir.json.zio.jsonBody
+import sttp.tapir.server.ServerEndpoint
+import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
+import zio.*
+import zio.http.Server
+import zio.json.{DeriveJsonCodec, JsonCodec}
+
+import scala.collection.mutable
+
+object TapirDemo extends ZIOAppDefault {
+
+  private val simplestEndpoint = endpoint
+    .tag("simple")
+    .name("simple")
+    .description("Simplest endpoint possible.")
+    .get
+    .in("simple")
+    .out(plainBody[String])
+    .serverLogicSuccess[Task](_ => ZIO.succeed("All good!"))
+
+  private val simpleServerProgram = Server.serve(
+    ZioHttpInterpreter(
+      ZioHttpServerOptions.default
+    ).toHttp(simplestEndpoint)
+  )
+  private val db: mutable.Map[Long, Job] = mutable.Map(
+    1L -> Job(1L, "Instructor", "rockthejvm.com", "Rock the JVM")
+  )
+
+  private val getAllEndpoint: ServerEndpoint[Any, Task] = endpoint
+    .tag("jobs")
+    .name("getAll")
+    .description("Get all jobs.")
+    .in("jobs")
+    .get
+    .out(jsonBody[List[Job]])
+    .serverLogicSuccess(_ => ZIO.succeed(db.values.toList))
+
+  private val createEndpoint: ServerEndpoint[Any, Task] = endpoint
+    .tag("jobs")
+    .name("create")
+    .description("Create a job.")
+    .in("jobs")
+    .post
+    .in(jsonBody[CreateJobRequest])
+    .out(jsonBody[Job])
+    .serverLogicSuccess(request =>
+      ZIO.succeed {
+        val newId  = db.keys.max + 1
+        val newJob = Job(newId, request.title, request.url, request.company)
+        db += (newId -> newJob)
+        newJob
+      }
+    )
+
+  private val getByIdEndpoint: ServerEndpoint[Any, Task] = endpoint
+    .tag("jobs")
+    .name("getById")
+    .description("Get job by id.")
+    .in("jobs" / path[Long]("id"))
+    .get
+    .out(jsonBody[Option[Job]])
+    .serverLogicSuccess(id => ZIO.succeed(db.get(id)))
+
+  private val serverProgram = Server.serve(
+    ZioHttpInterpreter(
+      ZioHttpServerOptions.default
+    ).toHttp(List(createEndpoint, getByIdEndpoint, getAllEndpoint))
+  )
+
+  override def run: ZIO[ZIOAppArgs & Scope, Any, Any] = serverProgram.provide(
+    Server.default
+  )
+}
+
+case class Job(
+    id: Long,
+    title: String,
+    url: String,
+    company: String
+)
+
+object Job {
+  given codec: JsonCodec[Job] = DeriveJsonCodec.gen[Job] // Macro-based JSON codec (generated).
+}
+
+case class CreateJobRequest(
+    title: String,
+    url: String,
+    company: String
+)
+
+object CreateJobRequest {
+  given codec: JsonCodec[CreateJobRequest] = DeriveJsonCodec.gen[CreateJobRequest]
+}
